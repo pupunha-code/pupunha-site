2befb1d9987701e45446af4d1983aa282da5f7d1
alessandrordgs <ale.silva1120@gmail.com>
2025-07-09 20:25:47 -0300

    feat(devs): add amapa developers list and rename page
    - Add devs-amapa.ts with developer profiles
    - Rename github to devs page
    - Include avatar display

---
+++ devs-amapa.ts
@@ -0,0 +1,15 @@
+type Dev = {
+  name: string;
+  github: string;
+};
+export const devs: Dev[] = [
+  {
+    "name": "Alessandro Rodrigues",
+    "github": "https://github.com/alessandrordgs",
+  },
+  {
+    "name": "Luma montes",
+    "github": "https://github.com/lumamontes",
+
+  } 
+]
