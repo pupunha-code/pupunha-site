c5afc6842840d15563f7271a53503a8556b30858
lumamontes <lumagoesmontes@gmail.com>
2025-10-12 01:26:50 -0300

    feat: start new redesign

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  },
+
   output: 'static',
   site: 'https://landing.pages.dev',
+  integrations: [icon()],
 });
